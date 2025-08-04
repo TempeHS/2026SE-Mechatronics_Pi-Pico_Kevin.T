bd626286cdc932e758d436895b3a1f1d3b466b34
Kevin T <kevin.tran120@education.nsw.gov.au>
2025-08-04 11:55:44 +1000

    testing ultrasonic and servos
    werks

--- project/py_scripts/v03.py
+++ project/py_scripts/v03.py
@@ -19,7 +19,8 @@ from servo import Servo
 
 
 # create a PWM servo controller (16 - pin Pico)
-servo_pwm = PWM(Pin(16))
+servo_pwm_left = PWM(Pin(16))
+servo_pwm_right = PWM(Pin(15))
 
 # Set the parameters of the servo pulses, more details in the "Documentation" section
 freq = 50
@@ -28,18 +29,16 @@ max_us = 2500
 dead_zone_us = 1500
 
 # create a servo object
-my_servo = Servo(
-    pwm=servo_pwm, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
+left_servo = Servo(
+    pwm=servo_pwm_left, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
+)
+right_servo = Servo(
+    pwm=servo_pwm_right, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
 )
 
 
 while True:
     # manually set the servo duty time
-    my_servo.set_duty(500)
-    time.sleep(2)
-
-    my_servo.set_duty(1500)
-    time.sleep(2)
-
-    my_servo.stop()
+    left_servo.set_duty(2500)
+    right_servo.set_duty(500)
     time.sleep(2)
