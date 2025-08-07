0f6656a03638ef6776f15a6db59baf191717d570
Kevin T <kevin.tran120@education.nsw.gov.au>
2025-08-07 15:06:53 +1000

    more movemnet class
    turn left is geeked and maybe it will fix itself

--- project/lib/movement.py
+++ project/lib/movement.py
@@ -1,23 +1,35 @@
-from machine import Pin, PWM
-from servo import Servo
-
-
-servo_pwm = PWM(Pin("pin"))
-freq = 50
-min_us = 500
-max_us = 2500
-dead_zone_us = 1500
-
-left_servo = Servo(
-    pwm=servo_pwm, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
-)
-
-
 class Movement:
     def __init__(self, left_servo, right_servo, debug):
         self.__left_servo = left_servo
         self.__right_servo = right_servo
         self.__debug = debug
 
+    def stop(self):
+        if self.__debug:
+            print("stopping")
+        self.__left_servo.stop()
+        self.__right_servo.stop()
+
     def forwards(self):
-        
+        if self.__debug:
+            print("moving forwards")
+        self.__left_servo.set_duty(2000)
+        self.__right_servo.set_duty(1000)
+
+    def backwards(self):
+        if self.__debug:
+            print("moving backwards")
+        self.__left_servo.set_duty(1000)
+        self.__right_servo.set_duty(2000)
+
+    def turn_right(self):
+        if self.__debug:
+            print("turning right")
+        self.__left_servo.set_duty(1600)
+        self.__right_servo.set_duty(1600)
+
+    def turn_left(self):
+        if self.__debug:
+            print("turning left")
+        self.__left_servo.set_duty(1300)
+        self.__right_servo.set_duty(1300)
